a3e7ac01eac4d0b2bf8b38e3f28ab231b88549b4
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2024-05-27 22:13:50 -0400

    chore: add song library skeleton

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,11 @@
 import ModeToggle from '@/components/dark-mode';
 import Settings from '@/components/settings';
+import SongLibrary from '@/components/song-library';
 
 export default function Header() {
   return (
     <header className="flex justify-end p-4 space-x-2">
+      <SongLibrary />
       <Settings />
       <ModeToggle />
     </header>
